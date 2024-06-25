eeb1a744f0a367ee4d48bdcc838ac5a6fb1a058f
shreyvishal <shreya.vishal@eminds.ai>
2024-06-25 20:20:39 +0530

    Added: First Test Case

--- scripts/index.js
+++ scripts/index.js
@@ -1,52 +1,16 @@
 import http from "k6/http";
 import { check, sleep } from "k6";
-
+import { htmlReport } from "https://raw.githubusercontent.com/benc-uk/k6-reporter/main/dist/bundle.js";
+import { generateUUID } from "./generateUUID.js";
 export const options = {
-  // A number specifying the number of VUs to run concurrently.
-  vus: 1,
-  // A string specifying the total duration of the test run.
-  duration: "15s",
-  iterations: 1
-
-  // The following section contains configuration options for execution of this
-  // test script in Grafana Cloud.
-  //
-  // See https://grafana.com/docs/grafana-cloud/k6/get-started/run-cloud-tests-from-the-cli/
-  // to learn about authoring and running k6 test scripts in Grafana k6 Cloud.
-  //
-  // cloud: {
-  //   // The ID of the project to which the test is assigned in the k6 Cloud UI.
-  //   // By default tests are executed in default project.
-  //   projectID: "",
-  //   // The name of the test in the k6 Cloud UI.
-  //   // Test runs with the same name will be grouped.
-  //   name: "script.js"
-  // },
-
-  // Uncomment this section to enable the use of Browser API in your tests.
-  //
-  // See https://grafana.com/docs/k6/latest/using-k6-browser/running-browser-tests/ to learn more
-  // about using Browser API in your test scripts.
-  //
-  // scenarios: {
-  //   // The scenario name appears in the result summary, tags, and so on.
-  //   // You can give the scenario any name, as long as each name in the script is unique.
-  //   ui: {
-  //     // Executor is a mandatory parameter for browser-based tests.
-  //     // Shared iterations in this case tells k6 to reuse VUs to execute iterations.
-  //     //
-  //     // See https://grafana.com/docs/k6/latest/using-k6/scenarios/executors/ for other executor types.
-  //     executor: 'shared-iterations',
-  //     options: {
-  //       browser: {
-  //         // This is a mandatory parameter that instructs k6 to launch and
-  //         // connect to a chromium-based browser, and use it to run UI-based
-  //         // tests.
-  //         type: 'chromium',
-  //       },
-  //     },
-  //   },
-  // }
+  scenarios: {
+    contacts: {
+      executor: "per-vu-iterations",
+      vus: 200,
+      iterations: 50,
+      maxDuration: "20m"
+    }
+  }
 };
 
 // The function that defines VU logic.
@@ -66,8 +30,8 @@ export default function () {
         action: "select",
         bap_uri: "https://bap-ps-network-vistaar.becknprotocol.io",
         version: "1.1.0",
-        transaction_id: "429839c0-23f8-46c0-a0dd-8efecc85d0ac",
-        message_id: "4e674703-1f0b-4d5f-b9f6-507ecf9584d0",
+        transaction_id: generateUUID(),
+        message_id: generateUUID(),
         location: {
           country: {
             name: "India",
@@ -100,9 +64,17 @@ export default function () {
   );
   check(res, {
     "success call": (r) => {
-      console.log(r);
-      return JSON.parse(r.body).responses.length > 0;
+      if (JSON.parse(r.body).responses) {
+        console.log(`Success:${JSON.parse(r.body).responses.length > 0}`);
+        return JSON.parse(r.body).responses.length > 0;
+      }
     }
   });
   sleep(1);
+}
+
+export function handleSummary(data) {
+  return {
+    "summary.html": htmlReport(data)
+  };
 }
